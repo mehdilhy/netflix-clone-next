8875f7980415f6f9a7d970145e14ee1b21030828
Medos <73502818+Medos-dev@users.noreply.github.com>
2021-08-24 16:34:35 +0100

    Trying to fix vercel not reading tailwind ( changed c letter on tailwind config and dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./Components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {},
   },
